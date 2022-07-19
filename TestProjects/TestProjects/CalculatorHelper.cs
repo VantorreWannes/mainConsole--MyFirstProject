f9232e1dc5fcdab8217f62f5b72af6a44c5faef3
Andrew Bauman <andrew.bauman332@gmail.com>
2022-07-19 11:08:39 -0500

    Added Function to evaluate simple expressions.

--- TestProjects/TestProjects/CalculatorHelper.cs
+++ TestProjects/TestProjects/CalculatorHelper.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace TestProjects
 {
     internal class CalculatorHelper
-    {   
+    {
         /// <summary>
         /// This method takes in a single line expression string, removes all whitespace, and separates expressions inside of parentheses.
         /// </summary>
@@ -22,7 +22,7 @@ namespace TestProjects
             for (int i = 0; i < tempArray1.Length; i++)
             {
                 tempArray2 = tempArray1[i].Split(")");
-                for(int j=0;j<tempArray2.Length; j++)
+                for (int j = 0; j < tempArray2.Length; j++)
                 {
                     SubExpressions.Add(tempArray2[j]);
                 }
@@ -30,6 +30,56 @@ namespace TestProjects
             }
 
             return SubExpressions;
+        }
+        
+        /// <summary>
+        /// Evaluates a "simple expression" with the +,-,*,/,^ operations. If the calculation goes wrong, the function will return int.MinValue.
+        /// Example: "2.317^4.2" will return a value of 34.09489.
+        /// </summary>
+        /// <param name="Expression"></param> A simple expression as a string in the form (number)(operator)(number)
+        /// <returns></returns> Returns the solution of the simple expression as a float.
+        private float EvaluateSimpleExpression(String Expression)
+        {
+            float Value1 = 0, Value2 = 0;
+            int OperatorLocation = 0;
+            String[] Operators = { "+", "-", "*", "/", "^" };
+            int index = 0;
+
+            while((OperatorLocation == 0) && index<Operators.Length)
+            {
+                if (Expression.IndexOf(Operators[index]) == -1)
+                index++;
+                else
+                    OperatorLocation = Expression.IndexOf(Operators[index]);
+            }
+            
+            Value1 = float.Parse(Expression.Substring(0, OperatorLocation));
+            Value2 = float.Parse(Expression.Substring(OperatorLocation+1));
+            String Operator = Expression.Substring(OperatorLocation, 1);
+
+            if (Operator.Equals("+"))
+            {
+                return Value1 + Value2;
+            }
+            else if (Operator.Equals("-"))
+            {
+                return Value1 - Value2;
+            }
+            else if (Operator.Equals("*"))
+            {
+                return Value1 * Value2;
+            }
+            else if (Operator.Equals("/"))
+            {
+                return Value1 / Value2;
+            }
+            else if (Operator.Equals("^"))
+            {
+                return (float)Math.Pow(Value1,Value2);
+            }
+
+            return float.MinValue;//If the function is used correctly, this statement should never occur.
+
         }
     }
 }
